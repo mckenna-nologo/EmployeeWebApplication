3f61134075b24d91ae3fe81839cd295c4fe9ea62
McKenna Makran <intern1@nologostudios.com>
2026-09-14 15:03:14 +0200

    added delet page (to be updated)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -162,6 +162,14 @@ namespace EmployeeWebApplication.Controllers
             return Content("<script>alert('Employee successfully updated!'); window.location.href='/Home/Index';</script>", "text/html");
         }
 
+        public IActionResult DeleteView()
+        {
+            // Ensure the DeleteView always receives a non-null model to avoid null reference in the Razor page
+            EmployeeDataViewModel model = new EmployeeDataViewModel();
+            model.Employees = EmployeeMockData.Employees;
+            return View(model);
+        }
+
         //delete
         [HttpPost]
         public IActionResult Delete(int employeeId)
